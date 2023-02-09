cd17b251bc7f05d0ed5e1e2febbf1879466b431f
0xJayPi <jpcampaya@gmail.com>
2023-02-09 11:37:24 +0100

    Deployment script updated

--- deploy/01-deploy-giveme7.js
+++ deploy/01-deploy-giveme7.js
@@ -1,11 +1,13 @@
 const { ethers, upgrades } = require("hardhat")
 
 module.exports = async function () {
+    console.log("------| Deploying Proxy with GiveMe7v1...")
     const giveMe7v1 = await ethers.getContractFactory("GiveMe7v1")
 
     const proxy = await upgrades.deployProxy(giveMe7v1)
     await proxy.deployed()
     console.log(`Proxy address is ${proxy.address}`)
+    console.log("Proxy Deployed!")
 }
 
 module.exports.tags = ["v1", "all"]
